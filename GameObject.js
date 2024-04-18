54ef24c0ba0ea746287e6d6e138c45b0e81d5ee2
MohamedAmirMamoon <mamoonmohamad0@gmail.com>
2024-04-17 21:16:12 -0700

    H

--- GameObject.js
+++ GameObject.js
@@ -6,7 +6,7 @@ class GameObject {
         this.direction = config.direction || "down";
         this.sprite = new Sprite({
             gameObject: this,
-            src: config.src || "/images/characters/people/dreamwalkersmall.png",
+            src: config.src || "/images/characters/people/Dreamwalker.png",
         });
 
     }
